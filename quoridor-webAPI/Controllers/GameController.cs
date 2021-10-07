628b31d346cf2c88afd5bccd24070ff069e46558
Mykhailo Kholmohorov <mykhailo1257@gmail.com>
2021-10-07 19:22:54 +0300

    web api template

---
+++ quoridor-webAPI/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quoridor_webAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        //Add game state (like Start, End, Active)
+    }
+}
---
+++ quoridor-webAPI/Data/Services/GameService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quoridor_webAPI.Data.Services
+{
+    public class GameService
+    {
+        // game state from start menu and etc
+    }
+}
